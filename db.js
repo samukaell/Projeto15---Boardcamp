47a046c2f6664685e6673fb5b3563b8a823d4ac1
samukaell <snoooydelta37@gmail.com>
2022-05-30 13:41:25 -0300

    deploy heroku

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
-import pg from "pg";
+import pg from 'pg';
 
-const {Pool} = pg;
-const db = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "sam123", // nao fica assim em um projeto de verdade
-  database: "bordcamp"
-});
+const { Pool } = pg;
 
-export default db;
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+
+const connection = new Pool(databaseConfig);
+
+export default connection;
